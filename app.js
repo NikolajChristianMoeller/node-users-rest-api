40aeae67fb3726c17682413c8b655d9f0bf9c988
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-09-04 17:54:22 +0200

    mjnj

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3333;
+const port = 3000;
 
 app.use(express.json()); // to parse JSON bodies
 app.use(cors());
